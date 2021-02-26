a4e7649354f8671d05c302718d5634e129d72db1
m4nh <degregorio.daniele@gmail.com>
2021-02-26 14:06:59 +0100

    Environment placeholders example

--- examples/environment_variables/main.py
+++ examples/environment_variables/main.py
@@ -1,6 +1,15 @@
 from choixe.configurations import XConfig
 import rich
 
+##################################
+# SETs ENVIRONMENT VARIABLE FIRST
+# to pass XConfig check
+#
+# > export V0=0.0
+# > export V1=2.0
+# > export V2=3.0
+#
+##################################
 
 cfg = XConfig(filename='cfg.yml')
 cfg.check_available_placeholders(close_app=True)
