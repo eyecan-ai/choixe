32bb176545ba150448e5ed2d174904c3cac26997
Daniele De Gregorio <degregorio.daniele@gmail.com>
2021-11-09 22:21:20 +0100

    Sweeper example

--- examples/configurations/sweeps/main.py
+++ examples/configurations/sweeps/main.py
@@ -3,8 +3,19 @@ import rich
 
 from choixe.placeholders import Placeholder
 from choixe.sweepers import Sweeper
+import tempfile
+from pathlib import Path
 
 cfg = XConfig(filename="cfg.yml")
 
+# Create sweeps from single cfg
 sweeped_cfgs = cfg.sweep()
-rich.print(sweeped_cfgs)
+
+folder = Path(tempfile.mkdtemp())
+for idx, cfg in enumerate(sweeped_cfgs):
+    rich.print("Sweep", "-" * 10)
+    rich.print(cfg)
+    filename = folder / f"sweep_{str(idx).zfill(5)}.yml"
+    cfg.save_to(filename=filename)
+
+rich.print("Output saved to:", folder)
